c53682a844d74cc2b915e220c4786e3caa960ded
Cody Phillips <cphill38@yahoo.com>
2019-09-25 15:15:38 -0500

    Completing restaurant feature

--- restaurant-management-service/src/main/java/com/revature/restaurantmanagementservice/controller/MenuController.java
+++ restaurant-management-service/src/main/java/com/revature/restaurantmanagementservice/controller/MenuController.java
@@ -1,8 +1,6 @@
 package com.revature.restaurantmanagementservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.restaurantmanagementservice.model.MenuItem;
 import com.revature.restaurantmanagementservice.service.MenuService;
-import com.revature.restaurantmanagementservice.service.RestaurantService;
 
 @RestController
 @RequestMapping("/menu")
@@ -18,9 +15,6 @@ public class MenuController {
 
 	@Autowired
 	MenuService menuService;
-	@Autowired
-	RestaurantService restaurantService;
-
 	
 	@PutMapping("/item")
 	public void updateMenuItem(@RequestBody MenuItem menuItem) {
